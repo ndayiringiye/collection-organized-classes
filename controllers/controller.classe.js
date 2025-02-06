751ec1b9c965c97b24a99ec5abeb6476327b8769
ndayiringiye david <ndayiringiyedavid120@gmail.com>
2025-02-06 10:16:32 +0200

    single classe tracked

--- controllers/controller.classe.js
+++ controllers/controller.classe.js
@@ -1,18 +1,34 @@
+import mongoose from "mongoose";
 import Classe from "../models/classesModel.js";
 
-export const createClasse = async (req , res) =>{
-      try {
-        const classes = await Classe.create(req.body)
-        res.status(200).json(classes)
-      } catch (error) {
-        res.status(500).json({message : error.message})
-      }
+export const createClasse = async (req, res) => {
+  try {
+    const classes = await Classe.create(req.body)
+    res.status(200).json(classes)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
- export const getAllClasse = async (req ,res) =>{
-     try {
-       const classes = await Classe.find({});
-        res.status(200).json(classes);
-     } catch (error) {
-      res.status(500).json({message: error.message});
-     }
- }
+export const getAllClasse = async (req, res) => {
+  try {
+    const classes = await Classe.find({});
+    res.status(200).json({ success: true, data: classes });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "server error" });
+  }
+}
+
+export const getSingle = async (req, res) => {
+  const { id } = req.params;
+
+  if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({success: false })
+  }
+  try {
+    
+      const classe = await Classe.findById(id)
+      res.status(200).json({success: true, data:classe})
+  } catch (error) {
+       res.status(500).json({success: false , error: "server error"})
+  }
+}
--- routes/classe.route.js
+++ routes/classe.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createClasse, getAllClasse } from "../controllers/controller.classe.js";
+import { createClasse, getAllClasse, getSingle } from "../controllers/controller.classe.js";
 
 const router = express.Router();
 
 router.post("/", createClasse);
 router.get("/", getAllClasse);
+router.get("/:id", getSingle);
 
-export default router
+export default router;
